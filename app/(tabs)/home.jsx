fb9b9fc684758ed85d7b57ca00f641e76794c509
Ranul01 <ranulhome@gmail.com>
2024-09-19 13:39:28 +0530

    Created header and slider

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/Home/Header'
+import Slider from '../../components/Home/Slider'
 
 export default function Home() {
     return (
         <View>
-            <Text>Home</Text>
+            {/* Header */}
+            <Header />
+
+            {/* Slider */}
+            <Slider />
+            {/* Category */}
+            {/* List of Services */}
+            {/* Add new Truck */}
         </View>
     )
 }
